7ade191a5087351f72a7c684b20ce19d00e33baa
wortit988 <106458453+wortit988@users.noreply.github.com>
2023-10-01 23:37:13 +0530

    Show Hide feature for Citymart page

--- src/components/Citymart.js
+++ src/components/Citymart.js
@@ -1,8 +1,40 @@
+import { useState } from "react"
+
+const Section = ({title, description}) => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    return (
+        <div className="border border-black p-2 m-2">
+            <h1 className="font-bold text-xl">{title}</h1>
+            {isVisible? <button onClick={() => setIsVisible(false)} className="cursor-pointer underline">Hide</button> :
+            <button onClick={() => setIsVisible(true)} className="cursor-pointer underline">Show</button>}
+           
+            
+            {isVisible&&<p>{description}</p>}
+        </div>
+    )
+}
+
 const Citymart = () => {
     return (
         <div>
-            <h1>Citymart</h1>
-            <h2>Order Something....</h2>
+            <h1 className="font-bold text-3xl p-2 m-2">Citymart</h1>
+            <Section 
+            title = {"About Citymart"} 
+            description ={"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."}
+            />
+
+            <Section 
+            title = {"Team Citymart"} 
+            description ={"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."}
+            />
+
+            <Section 
+            title = {"Careers"} 
+            description ={"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."}
+            />
+
         </div>
     )
 }
