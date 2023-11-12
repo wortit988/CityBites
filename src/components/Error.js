d88c60098c3e769e52aa42bea70ef92dd005a9e9
wortit988 <106458453+wortit988@users.noreply.github.com>
2023-11-12 19:37:53 +0530

    updating error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,9 +3,9 @@ const Error = () => {
     const err = useRouteError();
     return (
         <div>
-            <h1>Oops!</h1>
-            <h2>Something is not right!</h2>
-            <h2>{err.status + " : " + err.statusText}</h2>
+            <h1 className="font-bold text-5xl text-center py-6">Oops!</h1>
+            <h2 className="text-4xl text-center py-6">Something is not right!</h2>
+            <h2 className="font-bold text-center py-6 text-2xl">{err.status + " : " + err.statusText}</h2>
         </div>
     )
 };
